90710133ea854684f79ee87c44e13f7e67b4760e
Kyle Tulod <tulod.kyle.m@gmail.com>
2016-12-12 17:16:21 -0500

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,10 @@
 class CreateStudents < ActiveRecord::Migration
+
+# change and use the ActiveRecord create_table method within that method to create the table. The table should have a :name column with a type string.
+  def change
+   create_table :students do |t|
+     t.string :name
+   end
+ end
+
 end
